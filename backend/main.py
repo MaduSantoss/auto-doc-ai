2938523fe09f546a9e42b2b9785a9f36d0c026c6
Maria Eduarda <madusantoss1510@gmail.com>
2026-02-26 23:00:12 -0300

    update CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,10 @@ app = FastAPI(
 # Configuração de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://auto-doc-ai-six.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
